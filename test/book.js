05c668a30488dd77ff1d042323b5b5f17657797e
Alejandro <alejandro@gfisystems.ca>
2018-09-30 19:06:59 -0500

    test commit

--- test/book.js
+++ test/book.js
@@ -4,6 +4,7 @@ process.env.NODE_ENV = 'test';
 let mongoose = require("mongoose");
 let Book = require('../app/models/book');
 
+
 //Require the dev-dependencies
 let chai = require('chai');
 let chaiHttp = require('chai-http');
